64ddab55ea8a7d99bfd982b9d9e4feefbbbfa00a
Guillaume Augustoni <guillaume.augustoni@epfl.ch>
2021-05-14 13:17:05 +0200

    Initial orking version of website

--- src/assets/d3/index.ts
+++ src/assets/d3/index.ts
@@ -1,14 +1,39 @@
-import {geoEqualEarth, geoPath, geoContains} from 'd3-geo';
-import {json, select, csv, tsv, dsv, geoMercator} from 'd3';
-import {filter} from 'd3-array';
-export default {
-    geoEqualEarth,
-    geoMercator,
-    geoContains,
-    geoPath,
-    csv,
-    tsv,
-    dsv,
-    json,
-    select
-  };
+import { geoEqualEarth, geoPath, geoContains } from "d3-geo";
+import {
+  json,
+  select,
+  csv,
+  tsv,
+  dsv,
+  geoMercator,
+  scaleQuantile,
+  schemeBlues,
+  max,
+  interpolatePiYG,
+  interpolateOranges,
+  scaleSequential,
+  scaleLinear,
+  range,
+} from "d3";
+
+const d3 = {
+  geoEqualEarth,
+  geoMercator,
+  geoContains,
+  schemeBlues,
+  scaleSequential,
+  scaleLinear,
+  interpolatePiYG,
+  interpolateOranges,
+  scaleQuantile,
+  geoPath,
+  csv,
+  tsv,
+  max,
+  range,
+  dsv,
+  json,
+  select,
+};
+
+export default d3;
